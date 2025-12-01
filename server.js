df9a633d0f3e5a6379318a2473775e64fed0ba40
IonAndreea11 <ion.andreea@yahoo.com>
2025-12-01 23:14:49 +0100

    Update env

--- server.js
+++ server.js
@@ -14,11 +14,14 @@ dotenv.config();
 
 const { Pool } = pkg;
 const pool = new Pool({
-  user: String(process.env.DB_USER),
-  host: String(process.env.DB_HOST),
-  database: String(process.env.DB_NAME),
-  password: String(process.env.DB_PASSWORD),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const app = express();
